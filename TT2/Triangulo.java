eb10eb6067a57de0575daabb7fa2797162d27ad1
felipeSF10 <92943611+felipeSF10@users.noreply.github.com>
2022-12-19 19:44:13 +0100

    Cambios en la clase Triangulo

--- TT2/Triangulo.java
+++ TT2/Triangulo.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+
 public class Triangulo {
 
 	//Según sus lados
@@ -21,7 +23,30 @@ public class Triangulo {
 	 */
 	public Triangulo(int x, int y, int z, int a, int b, int c) {
 		// TODO - implement Triangulo.Triangulo
-		throw new UnsupportedOperationException();
+		this.x = x;
+		if(x < 1 || x > 100) {
+			throw new InputMismatchException("El lado no puede ser menor que 1 ni mayor que 100");
+		}
+		this.y = y;
+		if(y < 1 || y > 100) {
+			throw new InputMismatchException("El lado no puede ser menor que 1 ni mayor que 100");
+		}
+		this.z = z;
+		if(z < 1 || z > 100) {
+			throw new InputMismatchException("El lado no puede ser menor que 1 ni mayor que 100");
+		}
+		if(x + y <= z || y + z <= x || x + z <= y) {
+			throw new InputMismatchException("La suma de dos lados no pueden ser mas pequeño que el lado del tercero");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (a < 1 || b < 1 || c < 1 ) {
+			throw new InputMismatchException("Los angulos del triangulo tienen que ser mayor que 1");
+		}
+		if ((a + b + c) != 180) {
+			throw new InputMismatchException("Los angulos del triangulo tienen que sumar 180 grados");
+		}
 	}
 
 	public int getX() {
@@ -95,5 +120,6 @@ public class Triangulo {
 	public void setC(int c) {
 		this.c = c;
 	}
+
 
 }
